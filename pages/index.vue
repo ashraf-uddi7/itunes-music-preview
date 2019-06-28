876e20718128b58b68a9a2502dcd180e7f607716
Ashraf Uddin <ahafuddn22@gmail.com>
2019-06-27 23:12:39 -0300

    create site header

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,24 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        pontomais-musics
-      </h1>
-      <h2 class="subtitle">
-        Music library consuming apple itunes api.
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div>
+    <header>
+      <div class="container">
+        <div class="row">
+          <div class="c6-xs c4-md c3-lg">
+            <h2 class="logo">
+              Pontomais Music
+            </h2>
+          </div>
+          <div class="c6-xs c8-md c9-lg">
+            <input type="text">
+          </div>
+        </div>
       </div>
+    </header>
+    <div class="container">
+      <router-view />
     </div>
+    <div />
   </div>
 </template>
 
