661e83bc25e4b030e09f2ad62e1319c275a206aa
Ashraf Uddin <ahafuddn22@gmail.com>
2019-06-29 11:47:49 -0300

    Create albums component

---
+++ components/Ui/Pagination.vue
@@ -0,0 +1,92 @@
+<template>
+  <div v-if="size" class="container pagination">
+    <div class="row">
+      <div class="c6-xs">
+        <div class="row">
+          <div class="c5-xs">
+            <a :class="{ disabled: current == 1 }" @click.prevent="prev">
+              anterior
+            </a>
+          </div>
+          <div class="c2-xs">{{ current }}</div>
+          <div class="c5-xs">
+            <a :class="{ disabled: current == pages }" @click.prevent="next">
+              próxima
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="c6-xs">
+        {{ itemsFrom + 1 }} a {{ itemsTo + 1 }} de {{ size + 1 }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    size: {
+      type: Number,
+      default: 0
+    }
+  },
+  data: () => ({
+    current: 0,
+    pages: 1,
+    itemsPerPage: 10,
+    itemsFrom: 0,
+    itemsTo: 0
+  }),
+  watch: {
+    current() {
+      this.updateItems();
+    }
+  },
+  mounted() {
+    this.pages = Math.ceil(this.size / this.itemsPerPage);
+    this.current = 1;
+  },
+  methods: {
+    prev() {
+      if (this.current == 1) {
+        return;
+      }
+      this.current = this.current - 1;
+    },
+    next() {
+      if (this.current == this.pages) {
+        return;
+      }
+      this.current = this.current + 1;
+    },
+    updateItems() {
+      const itemsTo = this.current * this.itemsPerPage;
+      this.itemsTo = itemsTo > this.size ? this.size : itemsTo;
+      this.itemsFrom = itemsTo - this.itemsPerPage;
+      this.$emit("change", {
+        itemsFrom: this.itemsFrom,
+        itemsTo: this.itemsTo,
+        itemsPerPage: this.itemsPerPage,
+        current: this.current
+      });
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+.pagination {
+  padding: 20px 0;
+  .row {
+    a {
+      cursor: pointer;
+      font-weight: bold;
+      user-select: none;
+      &.disabled {
+        font-weight: normal;
+        cursor: no-drop;
+      }
+    }
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -9,19 +9,51 @@
             </h2>
           </div>
           <div class="c6-xs c8-md c9-lg">
-            <input type="text">
+            <input-component
+              :loading="loading"
+              placeholder="Busque um artista"
+              @change="search"
+            />
           </div>
         </div>
       </div>
     </header>
     <div class="container">
-      <router-view />
+      <albums />
     </div>
     <div />
   </div>
 </template>
 
+<script>
+export default {
+  components: {
+    albums: () => import("../components/Albums/Albums.vue"),
+    inputComponent: () => import("../components/Ui/Input.vue")
+  },
+  computed: {
+    loading() {
+      return this.$store.getters["store/loading"];
+    }
+  },
+  mounted() {
+    this.search({ target: { value: "bethoven" } });
+  },
+  methods: {
+    search(term) {
+      if (this.loading) {
+        return false;
+      }
+
+      if (term.length >= 3) {
+        this.$store.dispatch("store/getArtists", term.replace(/\s/g, "+"));
+      }
+    }
+  }
+};
+</script>
+
 <style lang="scss">
-@import '../assets/style/app.scss';
-@import '../assets/style/responsive.scss';
+@import "~/assets/style/app.scss";
+@import "~/assets/style/responsive.scss";
 </style>
