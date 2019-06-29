ad53bd7dd67103c46a98a7b6256390f4ff0e07ec
Ashraf Uddin <ahafuddn22@gmail.com>
2019-06-29 11:48:45 -0300

    add state management for albums, tracks and artists

---
+++ store/store.js
@@ -0,0 +1,75 @@
+export const state = () => ({
+  term: "",
+  artist: null,
+  artists: null,
+  albums: null,
+  songs: null,
+  loading: false
+});
+
+export const getters = {
+  term: ({ term }) => term,
+  artists: ({ artists }) => artists,
+  albums: ({ albums }) => albums,
+  songs: ({ songs }) => songs,
+  loading: ({ loading }) => loading
+};
+
+export const mutations = {
+  SET_TERM: (state, payload) => (state.term = payload),
+  SET_ARTIST: (state, payload) => (state.artist = payload),
+  SET_ARTISTS: (state, payload) => (state.artists = payload),
+  SET_ALBUMS: (state, payload) => (state.albums = payload),
+  SET_SONGS: (state, payload) => (state.songs = payload),
+  START_LOADING: state => (state.loading = true),
+  FINISH_LOADING: state => (state.loading = false)
+};
+
+export const actions = {
+  async getArtists({ commit, dispatch }, term) {
+    commit("START_LOADING");
+    try {
+      // get artists ids from term
+      const response = await this.$axios.$get(
+        `/api/search?term=${term}&entity=musicArtist&limit=5`
+      );
+
+      // stop searching if there is no results
+      if (!response.results || !response.results.length) {
+        return commit("FINISH_LOADING");
+      }
+
+      commit("SET_ARTISTS", response.results);
+      // transform artists list in a string with their ids.
+      // example: [{ artistId: 1, artistId: 2, artistId: 3}] => "1,2,3"
+      const artistsIds = response.results
+        .map(artist => artist.artistId)
+        .join(",");
+      dispatch("getAlbums", artistsIds);
+    } catch (error) {
+      return commit("FINISH_LOADING");
+    }
+  },
+  async getAlbums({ commit }, artistsIds) {
+    const response = await this.$axios.$get(
+      `/api/lookup?id=${artistsIds}&entity=album&limit=100`
+    );
+    // filter only collection types
+    const albums = response.results.filter(
+      album => album.wrapperType == "collection"
+    );
+
+    commit("SET_ALBUMS", albums);
+    commit("FINISH_LOADING");
+  },
+  async getSongs({ commit }, artistsIds) {
+    const response = await this.$axios.$get(
+      `/api/lookup?id=${artistsIds}&entity=song&limit=100`
+    );
+    // remove user info item
+    response.results.splice(0, 1);
+
+    commit("SET_SONGS", response.results);
+    commit("FINISH_LOADING");
+  }
+};
