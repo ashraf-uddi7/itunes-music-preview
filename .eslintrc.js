68c6edde14b0388cd917b2e4d65f0be3631c4367
Ashraf Uddin <ahafuddn22@gmail.com>
2019-06-29 11:46:37 -0300

    updating eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,16 +2,34 @@ module.exports = {
   root: true,
   env: {
     browser: true,
-    node: true
+    node: true,
+    "cypress/globals": true
   },
   parserOptions: {
     parser: 'babel-eslint'
   },
   extends: [
     '@nuxtjs',
-    'plugin:nuxt/recommended'
+    'plugin:nuxt/recommended',
+    'plugin:prettier/recommended',
+    "eslint:recommended",
+    'prettier',
+    'prettier/vue'
+  ],
+  plugins: [
+    'prettier',
+    "cypress"
   ],
   // add your custom rules here
   rules: {
+    "vue/component-name-in-template-casing": ["error", "PascalCase"],
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+  },
+  globals: {
+    $nuxt: true
+  },
+  parserOptions: {
+    parser: "babel-eslint"
   }
 }
